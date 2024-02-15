12220349b6868eb582d13319169a024245fe09d3
Mic-hvel <o.temitopemichael@gmail.com>
2024-02-15 19:30:47 +0100

    Switching to SCSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import About from "./compponents/About";
 import Footer from "./compponents/Footer";
 import Header from "./compponents/Header";
